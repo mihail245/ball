248be5c5ef414f495b1eb4adb31286dbb5e86990
mihail245 <privetbrawl3@gmail.com>
2026-07-01 01:18:05 +0300

    Update item.js

--- item.js
+++ item.js
@@ -1,5 +1,19 @@
+// item.js - список товаров
 const ITEMS_DATA = [
-    { id: "item_1782755611926_n6qg", name: "Чехол", points: 250 }
+    { id: "item_1", name: "Держатель для стакана, 2 шт", points: 250, completed: false },
+    { id: "item_2", name: "Духи мужские стойкие Блю седактон Blue seduction 10 мл", points: 250, completed: false },
+    { id: "item_3", name: "Антенна для SMA мужской 868 МГц 5,8 дБи", points: 250, completed: false },
+    { id: "item_4", name: "Адаптер для шланга", points: 250, completed: false },
+    { id: "item_5", name: "Топ красный", points: 250, completed: false },
+    { id: "item_6", name: "Топ черный", points: 250, completed: false },
+    { id: "item_7", name: "Топ белый", points: 250, completed: false },
+    { id: "item_8", name: "Измеритель зазора свечи зажигания 0.6-2.4 мм", points: 250, completed: false },
+    { id: "item_9", name: "Блеск для губ QIBEST34 цветной матовый", points: 250, completed: false },
+    { id: "item_10", name: "Губная грязь DIXI матовая бархатная", points: 250, completed: false },
+    { id: "item_11", name: "Очки солнцезащитные оранжевые", points: 200, completed: false },
+    { id: "item_12", name: "Очки солнцезащитные чёрные", points: 200, completed: false },
+    { id: "item_13", name: "Очки солнцезащитные белые", points: 200, completed: false },
+    { id: "item_14", name: "Очки солнцезащитные жёлтые", points: 200, completed: false },
+    { id: "item_15", name: "Ароматизатор автомобильный синий колокольчик", points: 250, completed: false }
 ];
-
 export default ITEMS_DATA;
